2773f499a21ce0263b977ceb2f3831e503870a47
Ejran Mustafoski <caschiamo1234@gmail.com>
2025-01-07 15:44:13 +0100

    implentata paginazione per spacecrafts, missions, facts e moons

--- src/app/components/spacecrafts/spacecrafts-list/spacecrafts-list.component.ts
+++ src/app/components/spacecrafts/spacecrafts-list/spacecrafts-list.component.ts
@@ -11,15 +11,63 @@ import { SpacecraftsComponent } from "../spacecrafts.component";
   styleUrl: './spacecrafts-list.component.css'
 })
 export class SpacecraftsListComponent implements OnInit{
-   spacecraftDetails!: SpacecraftDetails[];
-   constructor(private spacecraftService: SpacecraftService){
+  //  spacecraftDetails!: SpacecraftDetails[];
+  //  constructor(private spacecraftService: SpacecraftService){
   
-   }
+  //  }
+  // ngOnInit(): void {
+  //     let opd: Observable<SpacecraftDetails[]> = this.spacecraftService.getSpacecraftDetails();
+  //     opd.subscribe({
+  //       next: sts => this.spacecraftDetails = sts,
+  //      error: err => console.log(err)
+  //    });
+  //  }
+  spacecraftDetails: SpacecraftDetails[] = [];
+  page: number = 0;
+  readonly size: number = 6;
+  totalPages: number = 0;
+
+  constructor(private spacecraftService: SpacecraftService) {}
+
   ngOnInit(): void {
-      let opd: Observable<SpacecraftDetails[]> = this.spacecraftService.getSpacecraftDetails();
-      opd.subscribe({
-        next: sts => this.spacecraftDetails = sts,
-       error: err => console.log(err)
-     });
-   }
+      this.loadSpacecrafts();
+  }
+
+  private loadSpacecrafts(): void {
+      this.spacecraftService.getSpacecraftDetails(this.page, this.size).subscribe({
+          next: (response) => this.updateSpacecrafts(response.content, response.totalPages),
+          error: (err) => this.handleError(err)
+      });
+  }
+
+  private updateSpacecrafts(spacecrafts: SpacecraftDetails[], totalPages: number): void {
+      this.spacecraftDetails = spacecrafts;
+      this.totalPages = totalPages;
+  }
+
+  private handleError(error: any): void {
+      console.error('Errore nel caricamento dei dati', error);
+  }
+
+  nextPage(): void {
+      if (this.page < this.totalPages - 1) {
+          this.page++;
+          this.loadSpacecrafts();
+      }
+  }
+
+  prevPage(): void {
+      if (this.page > 0) {
+          this.page--;
+          this.loadSpacecrafts();
+      }
+  }
+
+  isFirstPage(): boolean {
+      return this.page === 0;
+  }
+
+  isLastPage(): boolean {
+      return this.page >= this.totalPages - 1;
+  }
 }
